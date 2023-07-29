b3ceb3fc6f2d08c3477e645d7868b972c8fed2b9
zarp <jeferson.ferreira@zarpsystem.com.br>
2023-07-29 17:37:08 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,34 @@
+import HomeScreen from "../src/screens/HomeScreens";
+
+export default HomeScreen;
+// import React from "react";
+// import Link from "../src/components/Link";
+// import Footer from "../src/components/patterns/Footer";
+
+
+// function Title({children, as}) {
+//     const Tag = as;
+    
+//     return (
+//         <React.Fragment>
+//             <Tag>{children}</Tag>
+//             <style jsx>{`
+//             ${Tag} {
+//                 color: red;
+//             }
+//             `}</style>
+//         </React.Fragment>
+//     )
+// }
+
+// export default function HomePage() {
+//     return (
+//         <div>
+//             <Title as={'h1'}>Alura cases - Home</Title>
+//             <Link href={'/faq'}>
+//                 Ir para a página de FAQ
+//             </Link>
+//             <Footer />
+//         </div>
+//     )
+// } 
